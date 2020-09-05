f3f0641bfa3b2f916e8c71aa7b76d38015966849
idleberg <git@idleberg.com>
2020-09-05 20:34:06 +0200

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 import typescript from '@rollup/plugin-typescript';
 
 const plugins = [
   commonjs(),
+  filesize(),
+  nodePolyfills(),
   typescript({
     allowSyntheticDefaultImports: true
   })
@@ -10,6 +14,10 @@ const plugins = [
 
 export default [
   {
+    external: [
+      'crypto',
+      'fs'
+    ],
     input: './src/sfv.ts',
     output: {
       dir: 'lib',
